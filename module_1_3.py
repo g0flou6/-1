b59d0c399fb282c73a3648b6d10a00658ac6d973
g0flou6 <goflou03@gmail.com>
2024-10-11 20:49:24 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Максим'
+print('Name: ' + name)
+age  = 20
+print('Age: ' + str(age))
+new_age = age + 1
+print('New age: ' + str(new_age))
+is_student = True
+print('Is student: ' + str(is_student))
