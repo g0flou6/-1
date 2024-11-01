e284b48fa70ae28423aeb85bfb1a9dadac4d748f
g0flou6 <goflou03@gmail.com>
2024-11-01 12:00:49 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,16 @@
+def password(n):
+    pword = ''
+    for i in range(1, n):
+        for j in range(2, n):
+            if j <= i:
+                continue
+            if n % (i + j) == 0:
+                pword += str(i) + str(j)
+    return pword
+
+first_number = int(input('Введите число от 3 до 20: '))
+if first_number < 3 or first_number > 20:
+    print('Неверное число')
+
+result = password(first_number)
+print('Ваш пароль:', result)
