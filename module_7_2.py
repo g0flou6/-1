f6b52dec889c9b6bbfb6de0273c3aa8466141ac1
g0flou6 <goflou03@gmail.com>
2024-12-16 19:43:37 +0300

    Update module_7_2.py

--- module_7_2.py
+++ module_7_2.py
@@ -6,10 +6,9 @@ info = [
     ]
 
 def custom_write(file_name, strings: list):
-    file = open(file_name, 'a', encoding= 'utf-8')
-    for string in strings:
-        file.write(f'{string}\n')
-    file.close()
+    with open(file_name, 'w', encoding= 'utf-8') as file:
+        for string in strings:
+            file.write(f'{string}\n')
 
     with open(file_name, 'r', encoding= 'utf-8') as read_file:
         strings_positions = {}
@@ -25,4 +24,6 @@ def custom_write(file_name, strings: list):
 
 
 
-print(custom_write('test.txt', info))
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
